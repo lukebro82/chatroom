665d88b0dc9c925cc8448dad49b62f34e235678c
Lucas <lucas.82@hotmail.com.ar>
2024-05-21 10:06:35 -0300

    deploy 1.1

--- frontend/index.js
+++ frontend/index.js
@@ -7,7 +7,9 @@ require("./pages/bienvenidos.ts");
 require("./pages/chat.ts");
 require("./pages/signup.ts");
 require("./router");
-exports.API_BASE_URL = "http://localhost:3000";
+exports.API_BASE_URL = process.env.ENVIRONMENT == "DEV"
+    ? "http://localhost:3000"
+    : "https://chatroom-xi4g.onrender.com";
 (function () {
     (0, button_1.initButtonComp)();
     (0, header_1.initHeaderComp)();
